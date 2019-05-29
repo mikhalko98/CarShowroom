17ab8671fc55209eaaddacf96dde0d640b381029
mikhalko98 <nickolya1998@mail.ru>
2019-05-29 18:49:35 +0300

    Update request.sql

--- sql/request.sql
+++ sql/request.sql
@@ -1,19 +1,31 @@
-/*3.	Звіт, який містить витрати та прибутки на авто за вказаний в запиті період часу.*/
-SELECT cars.id, cars.mark, cars.model, sup.price AS purPrice,
+/*1.*/
+SELECT cars.id, cars.mark, cars.model, emp.first_name, emp.last_name, sul.date_supple as data, sup.price AS purPrice,
         cars.price AS selPrice, cars.price-sup.price AS earnings
         FROM cars
 	INNER JOIN supplies sup ON sup.id_car = cars.id
     INNER JOIN sales sul ON sul.id_car = cars.id
-WHERE cars.there_is = 0 AND sul.date_supple >= '2019-01-01' AND  sul.date_supple <= '2019-03-25'
+    INNER JOIN employees emp ON emp.id = sul.id_employee
+WHERE cars.there_is = 0 AND sul.date_supple >= '2019-01-01' AND  sul.date_supple <= '2019-03-25' ORDER BY emp.email
 
-/*4.	Звіт проданих авто за вказаний період часу, структурований по моделям.*/
+SELECT emp.id, emp.first_name, emp.last_name, COUNT(cars.id) AS count_car,
+       SUM(cars.price) AS sumPrCar,SUM(sup.price) AS sumPrSup ,SUM(cars.price-sup.price) AS sum
+       FROM cars
+	INNER JOIN supplies sup ON sup.id_car = cars.id
+    INNER JOIN sales sul ON sul.id_car = cars.id
+    INNER JOIN employees emp ON emp.id = sul.id_employee
+WHERE cars.there_is = 0 AND sul.date_supple >= '2019-01-01' AND  sul.date_supple <= '2019-03-25'
+GROUP BY emp.id
+ORDER BY emp.email
+
+
+/*2.*/
 SELECT cars.id, cars.mark, cars.model, COUNT(cars.model) AS count FROM cars
 	INNER JOIN supplies sup ON sup.id_car = cars.id
     INNER JOIN sales sul ON sul.id_car = cars.id
 WHERE cars.there_is = 0 AND sul.date_supple >= '2019-01-01' AND  sul.date_supple <= '2019-03-25'
 GROUP BY cars.model
 
-/*5.Річний звіт продажу за кожен місяць в указаний рік*/
+/*3.*/
 
 SELECT  YEAR(sul.date_supple) AS Year, MONTHNAME(sul.date_supple) AS Month,
         SUM(cars.price-sup.price) AS Earnings, COUNT(cars.id) AS Count_car
@@ -23,7 +35,7 @@ SELECT  YEAR(sul.date_supple) AS Year, MONTHNAME(sul.date_supple) AS Month,
 WHERE cars.there_is = 0 AND YEAR (sul.date_supple) = 2019
 GROUP BY Month
 
-/*Кількість проданих авто співробітником та отриманих коштів з цих авто */
+/*4.*/
 
 SELECT emp.id, emp.first_name, emp.last_name, COUNT(cars.id) AS count_car ,
        SUM(cars.price-sup.price) AS sum
@@ -34,7 +46,7 @@ SELECT emp.id, emp.first_name, emp.last_name, COUNT(cars.id) AS count_car ,
 WHERE cars.there_is = 0 AND sul.date_supple >= '2019-01-01' AND  sul.date_supple <= '2019-03-25'
 GROUP BY emp.id
 
-/*вивести споживачів які купили більше 1 авто за останній рік*/
+/*5.*/
 
 SELECT cust.first_name, cust.last_name, cust.email, cust.telephone FROM sales sal
     INNER JOIN customers cust ON cust.id = sal.id_costumer
